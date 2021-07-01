f6c87b95a844620a241ae5381d925ef5cd276a4b
Nagisa0824 <yuzutea0824@gmail.com>
2021-07-01 13:34:42 +0000

    login signin flex

--- app/controllers/finders_controller.rb
+++ app/controllers/finders_controller.rb
@@ -6,7 +6,7 @@ class FindersController < ApplicationController
       @users = User.looks(params[:search], params[:word])
      # 投稿検索の場合
     else
-      @posts = Post.looks(params[:search], params[:word])
+      @posts = Post.looks(params[:search], params[:word]).order(created_at: :desc).page(params[:page]).per(10)
     end
   end
 end
